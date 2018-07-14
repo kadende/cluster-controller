7658ed61287b7717512411df315271a821da541b
mwas <francismwangi152@gmail.com>
2018-07-14 08:08:34 +0300

    uncommented tests

--- plugin-manager/get_test.go
+++ plugin-manager/get_test.go
@@ -8,6 +8,7 @@ import (
 	"path"
 	"github.com/hashicorp/go-getter"
 	"strconv"
+	"path/filepath"
 )
 
 func TestGettingPluginPath(t *testing.T) {
@@ -60,58 +61,58 @@ func TestDownloadingProvider(t *testing.T) {
 		expectedResults expectedResults
 	}
 
-	testScenarios := [1]scenario{
+	testScenarios := [11]scenario{
 		// testing happy case
 		{plugin: loadPlugin{pluginName: "file", pluginType: "provider", version: "0.0.1"},
 			expectedResults: expectedResults{path: "provider/file_0.0.1.so"}},
 
-		//// testing path already exists
-		//{plugin: loadPlugin{pluginName: "file", pluginType: "provider", version: "0.0.1"},
-		//	expectedResults: expectedResults{errorMessage: "plugin already exists"}},
-		//
-		//
-		//// test downloading flavor plugin
-		//{plugin: loadPlugin{pluginName: "file", pluginType: "flavour", version: "0.0.1"},
-		//	expectedResults: expectedResults{path: "flavour/file_0.0.1.so"}},
-		//
-		//// test with a different version
-		//{plugin: loadPlugin{pluginName: "file", pluginType: "provider", version: "0.0.0"},
-		//	expectedResults: expectedResults{path: "provider/file_0.0.0.so"}},
-		//
-		//// test if version not specified downloads the latest version
-		//{plugin: loadPlugin{pluginName: "file", pluginType: "provider"},
-		//	expectedResults: expectedResults{path: "provider/file_latest.so"}},
-		//
-		//// test downloading via full github url
-		//{plugin: loadPlugin{url: "https://github.com/mwaaas/kadende-provider-file/releases/download/0.0.1/plugin.so?pluginName=my-custom-plugin.so",
-		//	pluginType: "provider"},
-		//	expectedResults: expectedResults{path: "provider/my-custom-plugin_latest.so"}},
-		//
-		//
-		//// downloading file that already exists
-		//{plugin: loadPlugin{url: "https://github.com/mwaaas/kadende-provider-file/releases/download/0.0.1/plugin.so?pluginName=my-custom-plugin.so",
-		//	pluginType: "provider"},
-		//	expectedResults: expectedResults{errorMessage: "plugin already exists"}},
-		//
-		//
-		//// testing with url that does not exist
-		//{plugin: loadPlugin{url: "https://github.com/mwaside/file-provider-sample/archive/file-provider-sample_v1.26.0.so",
-		//	pluginType: "provider"},
-		//	expectedResults: expectedResults{errorMessage: "bad response code: 404"}},
-		//
-		//// test using file path
-		//{plugin: loadPlugin{url: "file://" + filepath.Join(workingDir, "testDataPlugin", "file-provider.so"),
-		//	pluginType: "provider"},
-		//	expectedResults: expectedResults{path: "provider/file-provider_latest.so"}},
-		//
-		//// test with a file that does not exist
-		//{plugin: loadPlugin{url: "file:///path/does/not/exist", pluginType: "provider"},
-		//	expectedResults: expectedResults{errorMessage: "no such file"}},
-		//
-		//// testing with invalid plugin type
-		//// test using file path
-		//{plugin: loadPlugin{url: "file://" + filepath.Join(workingDir, "testDataPlugin", "file-provider_v0.0.1"),
-		//	pluginType: "abcdefga"}, expectedResults: expectedResults{errorMessage: "invalid plugin type"}},
+		// testing path already exists
+		{plugin: loadPlugin{pluginName: "file", pluginType: "provider", version: "0.0.1"},
+			expectedResults: expectedResults{errorMessage: "plugin already exists"}},
+
+
+		// test downloading flavor plugin
+		{plugin: loadPlugin{pluginName: "file", pluginType: "flavour", version: "0.0.1"},
+			expectedResults: expectedResults{path: "flavour/file_0.0.1.so"}},
+
+		// test with a different version
+		{plugin: loadPlugin{pluginName: "file", pluginType: "provider", version: "0.0.0"},
+			expectedResults: expectedResults{path: "provider/file_0.0.0.so"}},
+
+		// test if version not specified downloads the latest version
+		{plugin: loadPlugin{pluginName: "file", pluginType: "provider"},
+			expectedResults: expectedResults{path: "provider/file_latest.so"}},
+
+		// test downloading via full github url
+		{plugin: loadPlugin{url: "https://github.com/mwaaas/kadende-provider-file/releases/download/0.0.1/plugin.so?pluginName=my-custom-plugin.so",
+			pluginType: "provider"},
+			expectedResults: expectedResults{path: "provider/my-custom-plugin_latest.so"}},
+
+
+		// downloading file that already exists
+		{plugin: loadPlugin{url: "https://github.com/mwaaas/kadende-provider-file/releases/download/0.0.1/plugin.so?pluginName=my-custom-plugin.so",
+			pluginType: "provider"},
+			expectedResults: expectedResults{errorMessage: "plugin already exists"}},
+
+
+		// testing with url that does not exist
+		{plugin: loadPlugin{url: "https://github.com/mwaside/file-provider-sample/archive/file-provider-sample_v1.26.0.so",
+			pluginType: "provider"},
+			expectedResults: expectedResults{errorMessage: "bad response code: 404"}},
+
+		// test using file path
+		{plugin: loadPlugin{url: "file://" + filepath.Join(workingDir, "testDataPlugin", "file-provider.so"),
+			pluginType: "provider"},
+			expectedResults: expectedResults{path: "provider/file-provider_latest.so"}},
+
+		// test with a file that does not exist
+		{plugin: loadPlugin{url: "file:///path/does/not/exist", pluginType: "provider"},
+			expectedResults: expectedResults{errorMessage: "no such file"}},
+
+		// testing with invalid plugin type
+		// test using file path
+		{plugin: loadPlugin{url: "file://" + filepath.Join(workingDir, "testDataPlugin", "file-provider_v0.0.1"),
+			pluginType: "abcdefga"}, expectedResults: expectedResults{errorMessage: "invalid plugin type"}},
 	}
 
 	for index, scenario := range testScenarios{
